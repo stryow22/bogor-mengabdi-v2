3312658adc2bd35d0a7f9abdf9871d0260cd1228
Satriyo Witjaksono <satriyowitjaksono2205@gmail.com>
2024-03-18 01:09:46 +0700

    Initial commit from Create Next Stack

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import materialTheme from "../material-theme";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = React.useState(() => new QueryClient());
+  return (
+    <>
+      <ThemeProvider theme={materialTheme}>
+        <CssBaseline />
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools />
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default App;
